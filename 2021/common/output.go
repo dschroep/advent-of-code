7ab00260d82bc64f567a59f2fb4a152174787ec8
Daniel Schröpfer <daniel.schroepfer@jambit.com>
2021-12-04 16:30:41 +0100

    Improved: `OutputResults`

--- 2021/common/output.go
+++ 2021/common/output.go
@@ -5,8 +5,18 @@ import "fmt"
 // Formats and prints the results of a `day`.
 func OutputResults(day byte, firstResult string, secondResult string) {
 	fmt.Printf("[Day %d]\n", day)
+
 	fmt.Printf("\t[Level 1]\n")
-	fmt.Printf("\t\t%s\n", firstResult)
+	if firstResult != "" {
+		fmt.Printf("\t\t%s\n", firstResult)
+	} else {
+		fmt.Printf("\t\t-- No Result --\n")
+	}
+
 	fmt.Printf("\t[Level 2]\n")
-	fmt.Printf("\t\t%s\n", secondResult)
+	if secondResult != "" {
+		fmt.Printf("\t\t%s\n", secondResult)
+	} else {
+		fmt.Printf("\t\t-- No Result --\n")
+	}
 }
