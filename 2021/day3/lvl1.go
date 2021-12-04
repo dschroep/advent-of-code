d4b0d5cd49e7b83d17da10b2124045e047f19b75
Daniel Schröpfer <daniel.schroepfer@jambit.com>
2021-12-04 12:03:48 +0100

    Improved: Day 3, Level 1

--- 2021/day3/lvl1.go
+++ 2021/day3/lvl1.go
@@ -16,43 +16,42 @@ func solveLvl1() string {
 	}
 
 	// Idea of this solution:
-	// - Each input row is a binary number of 12 bits -> create slice `values` with size 12
-	// - For each '1' we add 1 to the position's value - for '0' we subtract 1 from the position's value
-	// - After iterating over every line we either have a positive or negative number for a position
-	// -> If a position's value is positive, '1' was more common
-	// -> If a position's value is negative, '0' was more common
-	values := make([]int, 12)
+	// - Each input row is a binary number of 12 bits -> create slice `commons` with size 12
+	// - For each '1' we add 1 to the position's "common" - for '0' we subtract 1
+	// - After iterating over every line we either have a positive or negative "common" for a position
+	// -> If a position's "common" is positive, '1' was more common
+	// -> If a position's "common" is negative, '0' was more common
+	commons := make([]int, 12)
 	for _, input := range inputs {
 		splitInput := strings.Split(input, "")
 
 		for index, bit := range splitInput {
 			if bit == "1" {
-				values[index]++
+				commons[index]++
 			} else {
-				values[index]--
+				commons[index]--
 			}
 		}
 	}
 
 	// Now we have our values, so we can assemble gamma and epsilon rate
-	binGammaRate := make([]string, 12)
-	binEpsilonRate := make([]string, 12)
-	for index, value := range values {
+	var binGammaRate, binEpsilonRate string
+	for _, value := range commons {
 		if value > 0 {
-			binGammaRate[index] = "1"
-			binEpsilonRate[index] = "0"
+			binGammaRate += "1"
+			binEpsilonRate += "0"
 		} else {
-			binGammaRate[index] = "0"
-			binEpsilonRate[index] = "1"
+			binGammaRate += "0"
+			binEpsilonRate += "1"
 		}
 	}
 
-	decGammaRate, err := strconv.ParseInt(strings.Join(binGammaRate, ""), 2, 0)
+	decGammaRate, err := strconv.ParseInt(binGammaRate, 2, 0)
 	if err != nil {
 		return "Could not parse binary gamma rate. Aborting."
 	}
 
-	decEpsilonRate, err := strconv.ParseInt(strings.Join(binEpsilonRate, ""), 2, 0)
+	decEpsilonRate, err := strconv.ParseInt(binEpsilonRate, 2, 0)
 	if err != nil {
 		return "Could not parse binary epsilon rate. Aborting."
 	}
