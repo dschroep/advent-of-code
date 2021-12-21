1dd17db364bb043f87f136db42a6d9d0e9a26bb9
Daniel Schröpfer <daniel.schroepfer@jambit.com>
2021-12-21 16:32:47 +0100

    Solved: Day 14, Level 1

--- 2021/day14/lvl1.go
+++ 2021/day14/lvl1.go
@@ -2,32 +2,33 @@ package day14
 
 import (
 	"fmt"
+	"math"
 
 	"github.com/dschroep/advent-of-code/common"
 )
 
 // Solves level 1 of day 14 and returns the result as printable message.
-// FIXME: 2828 seems to be too high and 243 too low...
 func solveLvl1() string {
 	inputs, err := common.GetFileInput(14)
 	if err != nil {
 		return "Could not open input file. Aborting."
 	}
 
-	polymer := inputs[0]
+	polymer := Polymer(inputs[0])
 
-	rules := make([]InsertionRule, 0)
-	for _, insertionRule := range inputs[2:] {
-		rules = append(rules, ruleFromString(insertionRule))
+	rules := make([]InsertionRule, len(inputs[2:]))
+	for ruleIndex, rule := range inputs[2:] {
+		rules[ruleIndex] = InsertionRule{rule[:2], rule[6:]}
 	}
 
 	for step := 0; step < 10; step++ {
-		polymer = apply(polymer, rules)
+		polymer = polymer.applyRules(rules)
 	}
 
-	elementQuantities := getElementQuantities(polymer)
-	var minQuantity, maxQuantity int
-	for _, quantity := range elementQuantities {
+	quantities := polymer.quantities()
+	// Note: The values for these variables are the right way around!
+	minQuantity, maxQuantity := math.MaxInt, 0
+	for _, quantity := range quantities {
 		if quantity < minQuantity {
 			minQuantity = quantity
 		} else if quantity > maxQuantity {
--- 2021/day14/types.go
+++ 2021/day14/types.go
@@ -1,53 +1,54 @@
 package day14
 
-import "strings"
-
-// Representation for "AB -> C", where "A" and "B" are the input, while "C" is the output.
+// Representation for "AB -> C", where "AB" is `input`, while "C" is `output`.
 type InsertionRule struct {
 	input  string
 	output string
 }
 
-// Takes an insertion `rule` in the format "AB -> C" and returns an instance of ``InsertionRule`` for it.
-func ruleFromString(rule string) InsertionRule {
-	return InsertionRule{
-		string(rule[0]) + string(rule[1]),
-		string(rule[6]),
+// Defines a single insertion that needs to be applied to a ``Polymer``.
+type Insertion struct {
+	// Index _after_ which this shall be made.
+	at int
+	// What shall be inserted.
+	output string
+}
+
+type Polymer string
+
+// Counts the amount of each element in `polymer` and returns it as `map[element]quantity`.
+func (polymer Polymer) quantities() map[rune]int {
+	quantities := map[rune]int{}
+	for _, element := range polymer {
+		quantities[element]++
 	}
+
+	return quantities
 }
 
 // Applies all `rules` to `polymer` and returns the result.
-func apply(polymer string, rules []InsertionRule) string {
-	newPolymer := polymer
+func (polymer Polymer) applyRules(rules []InsertionRule) Polymer {
+	insertions := []Insertion{}
 
-	for i := 0; i < len(polymer)-1; i++ {
+	// Gather all the `insertions`.
+	for elementIndex := 0; elementIndex < len(polymer)-1; elementIndex++ {
+		elements := string(polymer[elementIndex : elementIndex+2])
+
 		for _, rule := range rules {
-			if strings.Compare(rule.input, polymer[i:i+2]) == 0 {
-				// Increment `i` to prevent going over the insertion again and again and again...
-				i++
-				// Insert `rule.output` at this position.
-				newPolymer = newPolymer[:i] + rule.output + newPolymer[i:]
-				// No need to check the other rules if this one was applicable.
+			if elements == rule.input {
+				insertions = append(insertions, Insertion{elementIndex, rule.output})
+
+				// Checking all the other rules would be a waste of time at this point...
 				break
 			}
 		}
 	}
 
-	return newPolymer
-}
-
-// Returns a map[element name]quantity for `polymer`.
-func getElementQuantities(polymer string) map[rune]int {
-	result := make(map[rune]int)
-	for _, element := range polymer {
-		_, exists := result[element]
-		if exists {
-			result[element]++
-			continue
-		}
-
-		result[element] = 1
+	// Apply all `insertions` "simultaneously".
+	for insertionIndex := len(insertions) - 1; insertionIndex >= 0; insertionIndex-- {
+		insertion := insertions[insertionIndex]
+		polymer = polymer[:insertion.at+1] + Polymer(insertion.output) + polymer[insertion.at+1:]
 	}
 
-	return result
+	return polymer
 }
