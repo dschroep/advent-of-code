0c7265fa2b0dc28cecf658273258990002e2c518
Daniel Schröpfer <daniel.schroepfer@jambit.com>
2021-12-22 11:17:06 +0100

    Improved: Day 14, Level 1
    Got rid of unnecessary complexity caused by ``Insertion``.

--- 2021/day14/types.go
+++ 2021/day14/types.go
@@ -6,14 +6,6 @@ type InsertionRule struct {
 	output string
 }
 
-// Defines a single insertion that needs to be applied to a ``Polymer``.
-type Insertion struct {
-	// Index _after_ which this shall be made.
-	at int
-	// What shall be inserted.
-	output string
-}
-
 type Polymer string
 
 // Counts the amount of each element in `polymer` and returns it as `map[element]quantity`.
@@ -28,26 +20,17 @@ func (polymer Polymer) quantities() map[rune]int {
 
 // Applies all `rules` to `polymer` and returns the result.
 func (polymer Polymer) applyRules(rules []InsertionRule) Polymer {
-	insertions := []Insertion{}
-
-	// Gather all the `insertions`.
-	for elementIndex := 0; elementIndex < len(polymer)-1; elementIndex++ {
+	for elementIndex := len(polymer) - 2; elementIndex >= 0; elementIndex-- {
 		elements := string(polymer[elementIndex : elementIndex+2])
 
 		for _, rule := range rules {
 			if elements == rule.input {
-				insertions = append(insertions, Insertion{elementIndex, rule.output})
+				polymer = polymer[:elementIndex+1] + Polymer(rule.output) + polymer[elementIndex+1:]
 
 				// Checking all the other rules would be a waste of time at this point...
 				break
 			}
 		}
-	}
-
-	// Apply all `insertions` "simultaneously".
-	for insertionIndex := len(insertions) - 1; insertionIndex >= 0; insertionIndex-- {
-		insertion := insertions[insertionIndex]
-		polymer = polymer[:insertion.at+1] + Polymer(insertion.output) + polymer[insertion.at+1:]
 	}
 
 	return polymer
